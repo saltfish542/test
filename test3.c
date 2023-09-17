1517984b3d26aed90db8147acbd0380fd862ea5e
saltfish542 <145006699+saltfish542@users.noreply.github.com>
2023-09-17 21:21:11 +0800

    Add files via upload

---
+++ test3.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+////´òÓ¡10*10Ñî»ÔÈý½Ç
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	for (int i = 0; i < 10; i++)
+//	{
+//		for (int j = 0; j <= i; j++)
+//		{
+//			if (j == 0)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			else if (i == j)
+//			{
+//				arr[i][j] = 1;
+//			}
+//			else
+//			{
+//				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//			}
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
